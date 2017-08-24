726486f7f4f6de80f4846ca238334db4aa9f1590
Travis CI User <travis@example.org>
2017-08-24 09:31:45 +0000

    Documentation for hasufell/rust-libnotify@843fb0c

---
+++ implementors/std/error/trait.Error.js
@@ -0,0 +1,12 @@
+(function() {var implementors = {};
+implementors["gdk_pixbuf"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"glib/error/struct.Error.html\" title=\"struct glib::error::Error\">Error</a>",];
+implementors["glib"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"glib/error/struct.Error.html\" title=\"struct glib::error::Error\">Error</a>",];
+implementors["libc"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/std/sys/imp/ext/fs/trait.FileTypeExt.js
@@ -0,0 +1,12 @@
+(function() {var implementors = {};
+implementors["gdk_pixbuf"] = [];
+implementors["glib"] = [];
+implementors["libc"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
